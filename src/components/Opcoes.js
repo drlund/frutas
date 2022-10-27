7b7b212d0e501b97bbd862c525b9d4e4630d03fb
Paulo Cesar Souza Pereira <drlund@hotmail.com>
2022-10-26 21:23:32 -0300

    Primeiro commit

---
+++ src/components/Opcoes.js
@@ -0,0 +1,29 @@
+
+const Opcoes = [
+    {
+      label: "Maçã",
+      value: "maca",
+    },
+    {
+      label: "Manga",
+      value: "manga",
+    },
+    {
+      label: "Banana",
+      value: "banana",
+    },
+    {
+      label: "Abacaxi",
+      value: "abacaxi",
+    },
+    {
+      label: "Mamão",
+      value: "mamao",
+    },
+    {
+      label: "Caju",
+      value: "caju",
+    },
+  ];
+
+  export default Opcoes;
